5dbc10ab21df111fa8393ddd8586a851fdb0bff1
Otabek <otabekabdulazizov282@gmail.com>
2022-02-01 12:43:00 +0500

    form

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,90 @@
 <template>
-  <div>
-    <h2>Form Input</h2>
-    <app-Onoff v-model="switched"></app-Onoff>
-    <div>
-      <h3 v-if="switched">switched</h3>
-      <h3 v-else>not switched</h3>
-    </div>
+  <div class="container">
+    <form class="pt-3" @submit.prevent="onSubmit">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          class="form-control"
+          :class="{ 'is-invalid': $v.email.$error }"
+          @blur="$v.email.$touch()"
+          v-model="email"
+        />
+        <div class="invalid-feedback" v-if="!$v.email.required">
+          fill the email
+        </div>
+        <div class="invalid-feedback" v-if="!$v.email.email">
+          This should be email
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          class="form-control"
+          :class="{ 'is-invalid': $v.password.$error }"
+          @blur="$v.password.$touch()"
+          v-model="password"
+        />
+        <div class="invalid-feedback" v-if="!$v.password.minLength">
+          it is not enough number at least should be
+          {{ $v.password.$params.minLength.min }}. Now it is
+          {{ password.length }}
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="confirm">Confirm password</label>
+        <input
+          type="password"
+          id="confirm"
+          class="form-control"
+          :class="{ 'is-invalid': $v.confirmPassword.$error }"
+          @blur="$v.confirmPassword.$touch()"
+          v-model="confirmPassword"
+        />
+        <div class="invalid-feedback" v-if="!$v.confirmPassword.sameAs">
+          Password should match
+        </div>
+      </div>
+      <br />
+      <button class="btn btn-success" type="submit" :disabled="$v.$invalid">
+        Submit
+      </button>
+    </form>
   </div>
 </template>
 
 <script>
-import onOff from "./onOff.vue";
+import { required, email, minLength, sameAs } from "vuelidate/lib/validators";
+
 export default {
   data() {
     return {
-      switched: false,
+      email: "",
+      password: "",
+      confirmPassword: "",
     };
   },
-  components: {
-    appOnoff: onOff,
+  methods: {
+    onSubmit() {
+      console.log("Password", this.password);
+      console.log("Email", this.email);
+    },
+  },
+  validations: {
+    email: {
+      required,
+      email,
+    },
+    password: {
+      minLength: minLength(6),
+    },
+    confirmPassword: {
+      sameAs: sameAs("password"),
+    },
   },
 };
 </script>
